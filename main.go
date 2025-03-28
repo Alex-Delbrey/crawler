fa0481a7a401d0a80f896fdbdc0bf12ed9d711ba
Jandro <delbreyalex@gmail.com>
2025-03-27 20:13:22 -0400

    command args reader

--- main.go
+++ main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
-// "fmt"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
-	// inputBody := `
-	// 		<html>
-	// 			<body>
-	// 				<a href="/path/one">
-	// 					<span>Boot.dev</span>
-	// 				</a>
-	// 				<a href="https://other.com/path/one">
-	// 					<span>Boot.dev</span>
-	// 				</a>
-	// 			</body>
-	// 		</html>`
-	//
-	// str, err := GetURLsFromHTML(inputBody, "https://blog.boot.dev")
-	// if err != nil {
-	// 	fmt.Println("failed")
-	// 	return
-	// }
-	//
-	// fmt.Println(str)
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		return
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		return
+	} else {
+		fmt.Println("starting crawler of:", os.Args[1])
+	}
+
+	BASE_URL := os.Args[1]
+	htmlBody, err := getHTML(BASE_URL)
+	if err != nil {
+		fmt.Println("error with html")
+	}
+	fmt.Println(htmlBody)
+}
+
+func getHTML(rawURL string) (string, error) {
+	resp, err := http.Get(rawURL)
+	if resp.StatusCode >= 400 {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if contentType := resp.Header.Get("Content-Type"); contentType != "text/html" {
+		return "", err
+	}
+	htmlResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(htmlResp), nil
 }
