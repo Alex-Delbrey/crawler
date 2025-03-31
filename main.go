20bba5d0fc75b116c650b9bd2493f013feb9a93a
Jandro <delbreyalex@gmail.com>
2025-03-30 23:02:32 -0400

    started recursive crawl function

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -21,11 +24,9 @@ func main() {
 	}
 
 	BASE_URL := os.Args[1]
-	htmlBody, err := getHTML(BASE_URL)
-	if err != nil {
-		fmt.Println("error with html")
-	}
-	fmt.Println(htmlBody)
+	pages := make(map[string]int)
+	crawlPage(BASE_URL, BASE_URL, pages)
+	fmt.Println(pages)
 }
 
 func getHTML(rawURL string) (string, error) {
@@ -35,7 +36,7 @@ func getHTML(rawURL string) (string, error) {
 	}
 	defer resp.Body.Close()
 
-	if contentType := resp.Header.Get("Content-Type"); contentType != "text/html" {
+	if contentType := resp.Header.Get("Content-Type"); contentType != "text/html; charset=utf-8" {
 		return "ERROR: response Header is not of Content-Type `text/html`", err
 	}
 	htmlResp, err := io.ReadAll(resp.Body)
@@ -44,4 +45,55 @@ func getHTML(rawURL string) (string, error) {
 	}
 
 	return string(htmlResp), nil
+}
+
+// TODO
+// - properly recurse through child URLs
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	rawBase, err := url.Parse(rawBaseURL)
+	if err != nil {
+		fmt.Println("URL stdlib was not able to parse rawBaseURL: ", err)
+		return
+	}
+	currentURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		fmt.Println("URL stdlib was not able to parse rawCurrentURL: ", err)
+		return
+	}
+
+	if rawBase.Host != currentURL.Host {
+		return
+	}
+
+	normCurrentURL, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if _, ok := pages[normCurrentURL]; ok {
+		pages[normCurrentURL]++
+		return
+	} else {
+		pages[normCurrentURL] = 1
+	}
+
+	currentURLhtml, err := getHTML(rawCurrentURL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("CURRENTLY IN THIS URL'S HTML: ", currentURLhtml)
+
+	htmlReader := strings.NewReader(currentURLhtml)
+	treeNode, err := html.Parse(htmlReader)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if treeNode.FirstChild != nil && treeNode.LastChild.Data == "a" {
+		crawlPage(rawBaseURL, treeNode.FirstChild.Attr[0].Val, pages)
+	} else if treeNode.FirstChild != nil && treeNode.LastChild.Data == "a" {
+		crawlPage(rawBaseURL, treeNode.LastChild.Attr[0].Val, pages)
+	}
 }
